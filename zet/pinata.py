5b20762fa6b13522c2f718f721a0321719840f20
Anders Hoff <inconvergent@gmail.com>
2021-09-05 13:43:02 +0200

    crude example of pinning to pinata

---
+++ zet/pinata.py
@@ -0,0 +1,42 @@
+from json import load
+from pprint import pprint as pp
+import requests
+
+#  https://docs.pinata.cloud/api-pinning/pin-by-hash
+
+
+BASE_URL = 'https://api.pinata.cloud'
+PIN_BY_HASH = f'{BASE_URL}/pinning/pinByHash'
+
+
+def pin_request_body(cid, name):
+  #  TODO: skip name if missing, set creator as kv
+  if not name:
+    name = 'x'
+  name = name.strip()
+  return {
+    'pinataMetadata': {
+      'name': name,
+      'keyvalues': {
+        'zetv': 0,
+        'title': name
+      }
+    },
+    'hashToPin': cid
+  }
+
+class Pinata:
+  def __init__(self):
+    with open('/home/anders/.zet.json') as f:
+      jsn = load(f)
+    self.auth = jsn
+    self.headers = {'Authorization':  f'Bearer {jsn["jwt"]}'}
+
+  def pin(self, cid, name):
+    pp(pin_request_body(cid, name))
+    r = requests.post(PIN_BY_HASH,
+                      json=pin_request_body(cid, name),
+                      headers=self.headers)
+    pp(r.json())
+    return
+
--- zet/zet.py
+++ zet/zet.py
@@ -1,4 +1,5 @@
 import requests
+from zet.pinata import Pinata
 
 TZ_BASE_URL = 'https://api.better-call.dev/v1/tokens/mainnet/'
 CONTRACT = 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton'
@@ -20,25 +21,29 @@ def contract_token_info(token_id):
   r = requests.get(CONTRACT_TOKEN_URL, params={'token_id': token_id})
   return r.json()
 
+def strip_protocol(uri):
+  if uri is None:
+    return None
+  assert uri.startswith('ipfs://'), 'not a valid ipfs uri??'
+  return uri[7:]
+
 def get_cids(info):
-  #  res = [
-  #    info.get('thumbnail_uri'),
-  #  ] thumbnail is the hic logo
-  res = []
-  #  TODO: assert artifact_uri is in formats
-  res.extend(map(lambda o: o.get('uri', None), info['formats']))
-  return list(filter(bool, res))
+  return list(filter(bool,
+                     map(lambda o: strip_protocol(o.get('uri', None)),
+                         info['formats'])))
 
 
 class Zet:
   def __init__(self, adr):
     self.adr = adr
+    self.pinata = Pinata()
 
   def transfers(self):
+    '''all tokens transferred to or from adr(?)'''
     #  TODO: filter by contract?
     return _bcd(TZ_BASE_URL + f'transfers/{self.adr}')
 
-  def adr_tokens(self):
+  def cids(self):
     for o in self.transfers():
       token_id = o['token']['token_id']
       info = contract_token_info(token_id)
@@ -46,6 +51,18 @@ class Zet:
       info = info.pop()
       title = info['name']
       print(f'objkt# {token_id}: {title}')
-      for url in get_cids(info):
-        yield url
+      for cid in get_cids(info):
+        yield cid
+
+  def pin_all(self):
+    for o in self.transfers():
+      token_id = o['token']['token_id']
+      info = contract_token_info(token_id)
+      assert len(info) == 1, 'len is not 1. why?'
+      info = info.pop()
+      title = info['name']
+      print(f'objkt# {token_id}: {title}')
+      for cid in get_cids(info):
+        self.pinata.pin(cid, title)
+
 
